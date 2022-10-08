cfce65335ed7fb501461298235039436b38a0108
Christopher Cosma <cosma.5@osu.edu>
2022-10-08 12:49:46 -0400

    Add chore table

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from twilio.rest import Client
 from twilio.twiml.messaging_response import MessagingResponse
 from dotenv import load_dotenv
 import os
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, render_template, redirect
 
 load_dotenv()
 
@@ -27,6 +27,31 @@ def reply():
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", people=get_people_chores())
+
+def get_people_chores(sort=False):
+    people = [
+        {
+            'name': 'Chris',
+            'gb_points': 100,
+            'chore': 'Clean kitchen'
+        },
+        {
+            'name': 'Riley',
+            'gb_points': 101,
+            'chore': 'Clean bathroom'
+        }
+    ]
+
+    if sort:
+        people.sort(reverse=True, key=lambda person: person['gb_points'])
+    
+    return people
+
+@app.route("/mark_chore_done", methods=['POST'])
+def mark_chore_done():
+    person = request.form['person']
+    # Do something with person name
+    return redirect('/')
 
 app.run(debug=True, port=5005)
