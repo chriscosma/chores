4218227608cb88f7e8d6d7acdc465d742d3dfa5e
Christopher Cosma <cosma.5@osu.edu>
2022-10-08 15:07:00 -0400

    Add get_gbp method

--- models.py
+++ models.py
@@ -15,7 +15,17 @@ class Person(db.Model):
 
 class TaskHistory(db.Model):
     __tablename__ = "task_history"
-    person = db.Column(db.ForeignKey(Person.name), primary_key=True)
-    task = db.Column(db.ForeignKey(Task.type), primary_key=True)
+    person = db.Column(db.String, db.ForeignKey(Person.name), primary_key=True)
+    task = db.Column(db.String, db.ForeignKey(Task.type), primary_key=True)
     date_assigned = db.Column(db.DateTime, nullable=False,
         default=datetime.now(timezone('EST')))
+    
+    @classmethod
+    def get_gbp(person_name: str):
+        gbp = 0
+        history = TaskHistory.query.filter_by(person=person_name)
+        for row in history:
+            task = Task.query.filter_by(type=row.task)
+            gbp += task.base_points
+        
+        return gbp
